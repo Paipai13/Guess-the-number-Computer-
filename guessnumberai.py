1e14ba1a84ca0146376363f2039663cec78ee4f9
Paipai13 <43116855+Paipai13@users.noreply.github.com>
2022-07-12 04:45:20 +0000

    Initial commit

---
+++ guessnumberai.py
@@ -0,0 +1,17 @@
+import random
+
+def guess(x):
+  random_number = random.randint(1,x)
+  guess= 0
+  while guess != random_number:
+    guess=int(input(f'I\'m thinking of a number, guess a number between 1 and {x}: '))
+    if guess < random_number:
+      print("Too low, guess again! ")
+    elif guess > random_number:
+      print("Too high, guess again! ")
+
+  print(f'Correct! The Number I was thinking of was  {random_number} ')
+      
+guess (10)
+
+# https://www.youtube.com/watch?v=8ext9G7xspg
